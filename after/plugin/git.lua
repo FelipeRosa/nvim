c7435383bf21a4c2ece0d0fc7fea1ac3c3c94b56
Felipe Rosa <felipe.sgrosa@gmail.com>
2023-10-24 00:41:19 -0300

    Fix gitsigns keymaps

--- after/plugin/git.lua
+++ after/plugin/git.lua
@@ -5,13 +5,13 @@ gs.setup({
 		local bufopts = { buffer = bufnr }
 
 		-- Hunk motion
-		vim.keymap.set("n", "[c", function()
+		vim.keymap.set("n", "]c", function()
 			vim.schedule(function()
 				gs.next_hunk()
 			end)
 		end, bufopts)
 
-		vim.keymap.set("n", "]c", function()
+		vim.keymap.set("n", "[c", function()
 			vim.schedule(function()
 				gs.prev_hunk()
 			end)
