cc3c0240891d8360b2fa970e6d4b8262fa3b5015
Felipe Rosa <felipe.sgrosa@gmail.com>
2023-10-26 13:16:25 -0300

    Refactor plugin loading

---
+++ lua/fsgr/plugins/themes.lua
@@ -0,0 +1,8 @@
+return {
+	{ "Mofiqul/dracula.nvim" },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+	},
+}
---
+++ lua/fsgr/plugins/ui.lua
@@ -0,0 +1,12 @@
+return {
+	{ "nvim-tree/nvim-tree.lua" },
+	{
+		"nvim-lualine/lualine.nvim",
+		requires = {
+			"nvim-tree/nvim-web-devicons",
+			opt = true,
+		},
+	},
+	{ "nvim-tree/nvim-web-devicons" },
+	{ "stevearc/dressing.nvim" },
+}
