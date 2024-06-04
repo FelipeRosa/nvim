7f4c29d84959cff502ccf0ba664e2e19676d2f0f
Felipe Rosa <felipe.sgrosa@gmail.com>
2024-06-04 11:59:44 -0300

    Update deps

--- lua/fsgr/plugins.lua
+++ lua/fsgr/plugins.lua
@@ -40,6 +40,7 @@ M._plugins = {
 		-- Python
 		{
 			"linux-cultist/venv-selector.nvim",
+			branch = "regexp",
 			dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
 			event = "VeryLazy",
 		},
